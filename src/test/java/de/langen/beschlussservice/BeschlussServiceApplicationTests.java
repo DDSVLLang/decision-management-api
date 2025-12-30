963dcea1a65e0b92c8ce3296f0bcd0920ec69301
sghyac1219 <yacine.sghairi@pass-consulting.com>
2025-12-30 15:52:42 +0100

    "Initialize Spring Boot project with PostgreSQL configuration, Flyway, and profiles for different environments. Added application properties, test class, and Git configuration files."

---
+++ src/test/java/de/langen/beschlussservice/BeschlussServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.langen.beschlussservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeschlussServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
