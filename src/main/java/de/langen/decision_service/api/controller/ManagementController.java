28d1a90cc21f14f6d98db77b8d6abe315c5bd9ef
sghyac1219 <yacine.sghairi@pass-consulting.com>
2026-01-23 20:45:25 +0100

    Add `@PreAuthorize` annotations to restrict access to admin-only endpoints in `ManagementController`

--- src/main/java/de/langen/decision_service/api/controller/ManagementController.java
+++ src/main/java/de/langen/decision_service/api/controller/ManagementController.java
@@ -6,6 +6,7 @@ import de.langen.decision_service.application.service.ManagementService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,21 +20,21 @@ public class ManagementController {
     private final ManagementService managementService;
 
     @GetMapping("/topic")
-    // @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @Operation(summary = "Get all topics")
     public ApiResponse<List<TopicResponse>> getAllTopics() {
         return ApiResponse.success(managementService.getAllTopics());
     }
 
     @GetMapping("/department")
-    // @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @Operation(summary = "Get all departments")
     public ApiResponse<List<DepartmentResponse>> getAllDepartments() {
         return ApiResponse.success(managementService.getAllDepartments());
     }
 
     @GetMapping("/committee")
-    // @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @Operation(summary = "Get all committees")
     public ApiResponse<List<CommitteeResponse>> getAllCommittees() {
         return ApiResponse.success(managementService.getAllCommittees());
